5b806da5c2cedc7029af197bb7af4ab201c3999c
DanCpp <danila.ushakov.08@mail.ru>
2026-09-08 22:44:22 +0700

    may be I have solution for Load the kernel lab

--- src/boot.asm
+++ src/boot.asm
@@ -2,25 +2,63 @@
 
 [ORG 0x7C00]
 
+; now booting from floppy disk
+; we would contain our kernel starting from 0x7E00
 
-start:
-  mov ds, ax
+.start:
+  ; init stack
+  cli
+  xor ax, ax
   mov ss, ax
   mov sp, 0x7C00
 
-  mov ah, 0x0E
-  mov si, msg
-print_char:
-  lodsb
-  int 0x10
-  test al, al
-  jnz print_char
+  ; initialize CHS = (0, 0, 2) 
+  xor cx, cx
+  xor dh, dh
+  mov cl, 2 ; sector 2
 
-loop:
-  jmp loop
+  ; initialize ES : BX = 0x7E00
+  mov si, 0x07E0
+  xor bx, bx
+.load_kernel: 
+  mov es, si
 
-msg:
-  db 'Hello, World!', 0x0A, 0x0D, 0
+
+  xor di, di ; problem counter
+.try_read:
+  mov ah, 0x02
+  mov al, 1 ; read 1 sector
+  int 0x13
+
+  jnc .read_success
+  inc di
+  cmp di, 4
+  jle .try_read
+  
+  int 0x18 ; get it back
+
+.read_success:
+
+  ; select next CHS coordinate
+  inc cl
+  cmp cl, 19 ; cl should be less than 19 (Floppy disk)
+  jl .done_indexing
+  mov cl, 1
+  inc dh
+  cmp dh, 2 ; dh should be less than 2 (Floppy disk)
+  jl .done_indexing
+  xor dh, dh
+  inc ch
+
+
+.done_indexing:
+
+  add si, 0x20
+  cmp si, (0x7E00 + KERNEL_SIZE) / 16
+  jl .load_kernel
+
+.stuck:
+  jmp .stuck
 
 times 510-($-$$) db 0
 dw 0xAA55
