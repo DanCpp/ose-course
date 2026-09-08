bb305b4a2da04c7a86e35f27904ec04d389376bf
DanCpp <danila.ushakov.08@mail.ru>
2026-09-08 23:11:19 +0700

    some better solution

--- src/boot.asm
+++ src/boot.asm
@@ -6,6 +6,8 @@
 ; we would contain our kernel starting from 0x7E00
 
 HIGH_LIMIT_SEGMENT equ (0x7E00 + KERNEL_SIZE) / 16
+SECTORS_PER_TRACK equ 18
+HEADS_PER_CYLINDER equ 2
 
 .start:
   ; init stack
@@ -15,9 +17,9 @@ HIGH_LIMIT_SEGMENT equ (0x7E00 + KERNEL_SIZE) / 16
   mov sp, 0x7C00
 
   ; initialize CHS = (0, 0, 2) 
-  xor cx, cx
-  xor dh, dh
-  mov cl, 2 ; sector 2
+  xor cx, cx                      ; cylinder 0
+  xor dh, dh                      ; head 0
+  mov cl, 2                       ; sector 2
 
   ; initialize ES : BX = 0x7E00
   mov si, 0x07E0
@@ -27,34 +29,35 @@ HIGH_LIMIT_SEGMENT equ (0x7E00 + KERNEL_SIZE) / 16
 
 .try_read:
   mov ah, 0x02
-  mov al, 1 ; read 1 sector
+  mov al, 1                       ; read 1 sector
   int 0x13
-
-  jnc .read_success
-  int 0x18 ; get it back
+  jc .read_failure
 
 .read_success:
-
-  ; select next CHS coordinate
-  inc cl
-  cmp cl, 19 ; cl should be less than 19 (Floppy disk)
+; select next CHS coordinate
+  inc cl                          ; sector + 1
+  cmp cl, SECTORS_PER_TRACK + 1   ; cl should be less than 19 (Floppy disk)
   jl .done_indexing
-  mov cl, 1
-  inc dh
-  cmp dh, 2 ; dh should be less than 2 (Floppy disk)
+  mov cl, 1                       ; reset sector to 1
+  inc dh                          ; heads + 1
+  cmp dh, HEADS_PER_CYLINDER + 1  ; dh should be less than 2 (Floppy disk)
   jl .done_indexing
-  xor dh, dh
-  inc ch
+  xor dh, dh                      ; reset head to 0
+  inc ch                          ; cylinder + 1
 
 
 .done_indexing:
 
-  add si, 0x20
-  cmp si, HIGH_LIMIT_SEGMENT
+  add si, 0x20                    ; increment segment will be +0x200
+  cmp si, HIGH_LIMIT_SEGMENT      ; compare to end
   jl .load_kernel
 
 .stuck:
   jmp .stuck
+
+
+.read_failure:
+  int 0x18                        ; get back to BIOS
 
 times 510-($-$$) db 0
 dw 0xAA55
